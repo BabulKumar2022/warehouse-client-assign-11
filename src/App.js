481a59d48cb3e12bcb37925bfeaf557efe528253
BabulKumar2022 <bb3583400@gmail.com>
2022-04-30 00:03:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './compnents/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './compnents/Home/Home';
+import Inventory from './compnents/Inventory/Inventory';
+import Blogs from './compnents/Blogs/Blogs';
+import AboutMe from './compnents/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='home' element={<Home></Home>}></Route>
+      <Route path='inventory' element={<Inventory></Inventory>}></Route>
+      <Route path='blogs' element={<Blogs></Blogs>}></Route>
+      <Route path='aboutme' element={<AboutMe></AboutMe>}></Route>
+    </Routes>
     </div>
   );
 }
---
+++ src/compnents/CustomLink/CustomLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CustomLink.css'
+
+const CustomLink = () => {
+    return (
+        <div>
+            <h1>Custom Link</h1>
+        </div>
+    );
+};
+
+export default CustomLink;
